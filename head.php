9f4d61a656f02d48ca71533f377679353490ecff
Your Name <you@example.com>
2025-10-23 11:39:10 +0100

    5

--- head.php
+++ head.php
@@ -1,4 +1,8 @@
 <head>
     <title>UHI Tunes</title>
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
 </head>
