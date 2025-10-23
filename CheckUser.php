1fa93dd42bd06a27163bde8a76784a0322503237
Your Name <you@example.com>
2025-10-23 11:26:11 +0100

    3

--- CheckUser.php
+++ CheckUser.php
@@ -45,7 +45,11 @@
   
     //	it's the first element - element 0 - from the row array if using line 35
   
-    echo '<br/>Associated User no is  ' . $UserNo; // or 'UserNo' if line 36
+    echo '<br/>Associated User no is  ' . $UserNo . '  <FORM METHOD="LINK" ACTION="AddCD.php">
+
+
+    <INPUT TYPE="submit" VALUE="Enter site">
+  </FORM>'; // or 'UserNo' if line 36
 //	echo '<h3>Associated User no is  '.$UserNo.'</h3>'; // or 'UserNo' if line 36
   
   } else
@@ -54,11 +58,7 @@
 
   ?>
 
-  <FORM METHOD="LINK" ACTION="AddCD.php">
 
-
-    <INPUT TYPE="submit" VALUE="Enter site">
-  </FORM>
 
 </body>
 <footer>
